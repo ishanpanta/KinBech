7873981ea10c9b91036c9e50650de13b792a2c9c
Ishan Panta <ishanpanta0124@gmail.com>
2021-08-21 10:30:45 +0545

    Hosting

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -19,4 +19,4 @@ urlpatterns = [
 
     path('django_plotly_dash/', include('django_plotly_dash.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
